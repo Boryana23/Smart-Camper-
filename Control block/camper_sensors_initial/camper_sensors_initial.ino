c07f56ae662a773b5a84bdd450ed0cc9f0328ce0
Boryana Stefanova <boriana.y.stefanova@gmail.com>
2023-02-23 07:21:42 +0200

    Implementing GPIO control

--- Control block/camper_sensors_initial/camper_sensors_initial.ino
+++ Control block/camper_sensors_initial/camper_sensors_initial.ino
@@ -1,108 +1,169 @@
+#include <DHTesp.h>
 #include <ThingsBoard.h>
-#include <DHT.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
-#include <ESPmDNS.h>
+#include <ESPmDNS.h>   
 
-#define WIFI_AP "BB"
-#define WIFI_PASSWORD "bb2304bb"
-#define TOKEN "St9imww2ZkCVmxUT4dwd"
-#define DHTPIN 26
-#define DHTTYPE DHT11
+#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
 
-char thingsboardServer[] = "zaimov.eu";
 
-WiFiClient wifiClient;
+#define WIFI_AP_NAME       "BB"
+#define WIFI_PASSWORD       "bb2304bb"
+#define TOKEN               "St9imww2ZkCVmxUT4dwd"
+#define THINGSBOARD_SERVER   "zaimov.eu"
+#define SERIAL_DEBUG_BAUD    115200
 
-DHT dht(DHTPIN, DHTTYPE);
-
-ThingsBoard tb(wifiClient);
+WiFiClient espClient;
+ThingsBoard tb(espClient);
 
 int status = WL_IDLE_STATUS;
-unsigned long lastSend;
+int gpioState[] = {0, 0, 0, 0};
+uint8_t leds_control[] = { 23, 19, 22, 21 };
+#define DHT_PIN 27
+DHTesp dht;
 
-void setup()
-{
-  Serial.begin(115200);
-  dht.begin();
-  delay(10);
-  InitWiFi();
-  lastSend = 0;
-}
 
-void loop()
-{
-  if ( !tb.connected() ) {
-    reconnect();
-  }
+int quant = 20;
+int led_delay = 1000;
+int send_delay = 2000;
 
-  if ( millis() - lastSend > 1000 ) { 
-    getData();
-    lastSend = millis();
-  }
+int led_passed = 0;
+int send_passed = 0;
 
-  tb.loop();
-}
+bool subscribed = false;
+int current_led = 0;
 
-void getData()
-{
-  Serial.println("Collecting temperature data.");
 
+
+RPC_Response processGetGpioState(const RPC_Data &data){
   
-  float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
+  String payload;
+  char buff[5];
+  StaticJsonDocument<200> doc;
 
- 
-  if (isnan(humidity) || isnan(temperature)) {
-    Serial.println("Failed to read from DHT sensor!");
+  Serial.println("Received the set GPIO RPC method");
+  
+ for (int i = 0; i < COUNT_OF(leds_control); i++) {
+    doc[String(i)] = gpioState[i];
+  }
+  serializeJson(doc, payload);
+  return RPC_Response(NULL, payload.c_str());  
+}
+
+RPC_Response processSetGpioState(const RPC_Data &data){
+  Serial.println("Received the get GPIO RPC method");
+
+  int pin = data["pin"];
+  bool enabled = data["enabled"];
+
+  if (pin < COUNT_OF(leds_control)) {
+    Serial.print("Setting LED ");
+    Serial.print(pin);
+    Serial.print(" to state ");
+    Serial.println(enabled);
+
+    digitalWrite(leds_control[pin], !enabled);
+    gpioState[pin] = enabled;
+  }
+
+  return RPC_Response(data["pin"], (bool)data["enabled"]);
+}
+
+RPC_Callback callbacks[] = {
+  { "setGpioStatus",    processSetGpioState },
+  { "getGpioStatus",    processGetGpioState }
+};
+
+void setup() {
+
+  Serial.begin(SERIAL_DEBUG_BAUD);
+  WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
+  void InitWiFi();
+  dht.setup(DHT_PIN, DHTesp::DHT11);
+
+  for (size_t i = 0; i < COUNT_OF(leds_control); ++i) {
+    pinMode(leds_control[i], OUTPUT);
+    digitalWrite(leds_control[i], HIGH);
+  }
+
+}
+
+void loop() {
+  delay(quant);
+
+  led_passed += quant;
+  send_passed += quant;
+
+  if (WiFi.status() != WL_CONNECTED) {
+    void reconnect();
     return;
   }
 
-  Serial.println("Sending information to ThingsBoard:");
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.println(" *C ");
+ 
+  if (!tb.connected()) {
+    subscribed = false;
+    Serial.print("Connecting to: ");
+    Serial.print(THINGSBOARD_SERVER);
+    Serial.print(" with token ");
+    Serial.println(TOKEN);
+    if (!tb.connect(THINGSBOARD_SERVER, TOKEN)) {
+      Serial.println("Failed to connect");
+      return;
+    }
+  }
 
-  tb.sendTelemetryFloat("temperature", temperature);
-  tb.sendTelemetryFloat("humidity", humidity);
+}
+
+  if (!subscribed) {
+    Serial.println("Subscribing for RPC...");
+
+      if (!tb.RPC_Subscribe(callbacks, COUNT_OF(callbacks))) {
+      Serial.println("Failed to subscribe for RPC");
+      return;
+    }
+
+    Serial.println("Subscribe done");
+    subscribed = true;
+  }
+
+  
+  if (send_passed > send_delay) {
+    Serial.println("Sending data...");
+
+    TempAndHumidity lastValues = dht.getTempAndHumidity();    
+    if (isnan(lastValues.humidity) || isnan(lastValues.temperature)) {
+      Serial.println("Failed to read from DHT sensor!");
+    } else {
+      tb.sendTelemetryFloat("Temperature", lastValues.temperature);
+      tb.sendTelemetryFloat("Humidity", lastValues.humidity);
+    }   
+
+    send_passed = 0;
+  }
+
+  //tb.loop();
 }
 
 void InitWiFi()
 {
-  Serial.println("Connecting to AP ...");
-  WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+  Serial.println("Connecting to Local network ...");
+  WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("Connected to AP");
+  Serial.println("Connected to Local network!");
 }
 
-
 void reconnect() {
-  
-  while (!tb.connected()) {
-    status = WiFi.status();
-    if ( status != WL_CONNECTED) {
-      WiFi.begin(WIFI_AP, WIFI_PASSWORD);
-      while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-      }
-      Serial.println("Connected to AP");
+  status = WiFi.status();
+  if ( status != WL_CONNECTED) {
+    WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
     }
-    Serial.print("Connecting to ThingsBoard ...");
-    if ( tb.connect(thingsboardServer, TOKEN) ) {
-      Serial.println( "[DONE]" );
-    } else {
-      Serial.print( "[FAILED]" );
-      Serial.println( " : retrying in 5 seconds]" );
-      
-      delay( 5000 );
-    }
+    Serial.println("Connected to Local network!");
   }
 }
